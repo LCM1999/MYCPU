ac34928c83bd1fc83562b424f2f06dd42f1bbd55
lcm <1137909852@qq.com>
2020-01-08 14:49:35 +0800

    add

--- work/@i@r/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity IR is
-    port(
-        CLK             : in     vl_logic;
-        ins             : in     vl_logic_vector(31 downto 0);
-        IRIns           : out    vl_logic_vector(31 downto 0)
-    );
-end IR;
--- work/@ins@mem/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity InsMem is
-    port(
-        InsAddr         : in     vl_logic_vector(31 downto 0);
-        InsData         : out    vl_logic_vector(31 downto 0)
-    );
-end InsMem;
